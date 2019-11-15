33f515a54c1b102d4729fe4bfce7b68b0316ddd9
Martin Horváth <pigfighter2@gmail.com>
2019-11-15 15:27:07 +0100

    Project, libary import

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Fogadóóra</title>
+
+	<!-- Libaries -->
+		<!-- jQuery -->
+		<script src="libs/jquery.js"></script>
+		<!-- Bootstrap -->	
+		<script src="libs/bootstrap/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="libs/bootstrap/bootstrap.min.css">
+		<!-- Chart.js -->
+		<script src="libs/Chart.min.js"></script>
+	
+
+	<!-- Styles -->
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	
+	<!-- JS -->	
+	<script src="js/script.js"></script>
+
+</head>
+<body>
+	
+</body>
+</html>
